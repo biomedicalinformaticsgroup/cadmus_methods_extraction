9d29b9c4cf47fc75cdeac3519209c019cf848421
Antoinelfr <83538367+Antoinelfr@users.noreply.github.com>
2025-11-23 12:09:30 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+   
+setuptools.setup(
+    name="cadmus",
+    version="0.3.15",
+    author="Jamie Campbell, Ian Simpson, Antoine Lain",
+    author_email="Jamie.campbell@igmm.ed.ac.uk, Ian.Simpson@ed.ac.uk, Antoine.Lain@ed.ac.uk",
+    description="This projects is to build full text retrieval system setup for generation of large biomedical corpora from published literature.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: LINUX/MACOS",
+    ],
+    install_requires=[
+'pandas',
+'numpy',
+'requests==2.26.0',
+'bs4',
+'tika==1.24',
+'urllib3',
+'wget',
+'biopython',
+'python-dateutil',
+'lxml',
+'IPython',
+'fuzzywuzzy',
+'papermage[dev,predictors,visualizers]',
+'ftfy',
+'pyarrow==20.0.0',
+],
+    python_requires='>=3.10'
+)
